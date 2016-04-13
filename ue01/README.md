f787088ef65a2e4bfb57bde89e822132dc19a8ec
Maxim Uhlemann <maxim.uhlemann@gmail.com>
2016-04-13 19:21:33 +0200

    4. a

---
+++ ue01/README.md
@@ -0,0 +1,59 @@
+# htw-algorithms_and_optimizations
+
+## maxim's branch
+
+https://www.npmjs.com/package/big-integer
+
+###Aufgabe 1:
+  Sprache - node.js
+  Aufruf - "node gcd.js a b"
+
+###Aufgabe 2:
+  TODO ...
+
+###Aufgabe 3:
+  Sprache - node.js
+  Aufruf - "node fibonaci.js n"
+
+###Aufgabe 4:
+
+####GCD
+    a: 12   b: 3
+    d: 3    u_d: 0  v_d: -3
+    Done in 2157μs
+    Required iterations 2
+
+    -----------------------
+
+    a: 16   b: 12
+    d: 4    u_d: 0  v_d: 0
+    Done in 2564μs
+    Required iterations 3
+
+    -----------------------
+
+    a: 120000   b: 3000
+    d: 3000 u_d: 1  v_d: -39
+    Done in 2401μs
+    Required iterations 2
+
+    -----------------------
+
+    a: 39088168 b: 1016
+    d: 8    u_d: 0  v_d: 0
+    Done in 2671μs
+    Required iterations 9
+
+    -----------------------
+
+    a: 89   b: 55           (consecutive fibonacci numbers)
+    d: 1    u_d: 0  v_d: 0
+    Done in 3175μs
+    Required iterations 10
+
+    -----------------------
+
+    a: 9227465  b: 14930352 (consecutive fibonacci numbers)
+    d: 1    u_d: 0  v_d: 0
+    Done in 3799μs
+    Required iterations 34
--- ue01/gcd.js
+++ ue01/gcd.js
@@ -4,7 +4,7 @@ var bigInt = require("big-integer")
  * Aufgabe 1
  */
 function extendedGCD(a, b) {
-  console.log(a, b)
+  var startTime = process.hrtime()
   if(typeof a.value == 'undefined' || typeof b.value == 'undefined') throw 'Input type error 1'
   if(a.value < 0 || b.value < 0) throw 'Input type error 2'
   if( !(( a.isPositive() && b.isPositive() ) && ( !a.eq(0) || !b.eq(0) )) ) throw 'Input type error 3'
@@ -32,19 +32,19 @@ function extendedGCD(a, b) {
     dUV = tempUV
 
   }
-
+  // source: http://stackoverflow.com/questions/11725691/how-to-get-a-microtime-in-node-js
+  var startTimeBig = bigInt(startTime[0] * 1000000).plus(bigInt(startTime[1]).divide(bigInt(1000)))
+  var currentTimeBig = bigInt(process.hrtime()[0] * 1000000).plus(bigInt(process.hrtime()[1]).divide(1000))
   return {
     k: d,
-    uv: dUV
+    uv: dUV,
+    time: currentTimeBig.minus(startTimeBig),
+    iterations: counter
   }
 }
 
-try {
-  var a = bigInt(process.argv[2])
-  var b = bigInt(process.argv[3])
-  console.log(extendedGCD(a,b))
-} catch(e) {
-  throw 'Input type error 0'
-}
+var a = bigInt(process.argv[2])
+var b = bigInt(process.argv[3])
+console.log(extendedGCD(a,b))
 
 module.exports = extendedGCD
