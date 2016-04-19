fb3225205c4c4470d10ed2ee338fa05be4f02801
Robert Wettstädt <robert.wett@gmail.com>
2016-04-19 21:36:16 +0200

    Dual sided fractals

--- ue02/fractal.c
+++ ue02/fractal.c
@@ -4,119 +4,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <assert.h>
+#include <sys/time.h>
 #include "vector.h"
+#include "svg.h"
 
-char svg[] = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='{{width}}px' height='{{height}}px' viewBox='{{minX}} {{minY}} {{width}} {{height}}' zoomAndPan='disable' preserveAspectRatio='none'>\n\t{{path}}\n</svg>";
-char path[] = "<path d='M {{a_x}} {{a_y}} L {{c_x}} {{c_y}} L {{e_x}} {{e_y}} L {{d_x}} {{d_y}} L {{b_x}} {{b_y}} z' fill='orange' stroke='black' stroke-width='3' />\n\t";
-
-char *newSvg;
-
-double smallestX;
-double smallestY;
-double biggestX;
-double biggestY;
-
-int replace_str(char *orig, char *rep, char *after) {
-    // printf("replace %s with %s\n", orig, rep);
-
-    static char buffer[4096];
-    char *p;
-
-    if(!(p = strstr(newSvg, orig)))
-        return 0;
-
-    strncpy(buffer, newSvg, p - newSvg);
-    buffer[p - newSvg] = '\0';
-
-    sprintf(buffer + (p - newSvg), "%s%s%s", rep, after, p + strlen(orig));
-    newSvg = malloc(strlen(buffer) + 1);
-    sprintf(newSvg, "%s", buffer);
-    return 1;
-}
-
-void replace (struct Vector *v, char *name) {
-
-    char rep[15];
-    char orig[19];
-
-    if (v->x < smallestX) smallestX = v->x;
-    if (v->x > biggestX) biggestX = v->x;
-    if (v->y < smallestY) smallestY = v->y;
-    if (v->y > biggestY) biggestY = v->y;
-
-    sprintf(rep, "%f", v->x);
-    sprintf(orig, "{{%s_x}}", name);
-    replace_str(orig, rep, "");
-
-    sprintf(rep, "%f", v->y);
-    sprintf(orig, "{{%s_y}}", name);
-    replace_str(orig, rep, "");
-
-}
+const double rad = M_PI / 180;
+unsigned int origAlpha;
+double sinAlpha;
 
 
-int fractalPart (struct Vector *a, struct Vector *b, int alpha, double h, int cycles) {
+int fractalPart (struct Vector *a, struct Vector *b, int alpha, double h, int depth) {
 
-    if (cycles > 100 || h < 1) return cycles;
-    int alphaNormalized = alpha * cycles;
+    if (depth > 10 || h < 0.1) return h;
 
     struct Vector *ab = newVector(b->x - a->x, b->y - a->y);
+    const double length_ab = vectorLength(ab);
 
     struct Vector *c = newVector(ab->y + a->x, -(ab->x - a->y));
     struct Vector *d = newVector((b->x + c->x) - a->x, (b->y + c->y) - a->y);
 
+    const double length_ce = fabs(h / sinAlpha);
+    const double e_x = length_ce * cos(-alpha * rad);
+    const double e_y = length_ce * sin(-alpha * rad);
+    struct Vector *e = newVector(c->x + e_x, c->y + e_y);
 
+    insertPath(a, b, c, d, e, depth);
 
-    double si = sin(-alphaNormalized * M_PI / 180);
-    double co = cos(-alphaNormalized * M_PI / 180);
+    h *= length_ce / length_ab;
+    ++depth;
 
-    double length_ce = fabs(h / sin(-alphaNormalized / cycles * M_PI / 180));
-    struct Vector *e;
-
-    double e_x = length_ce;
-    double e_y = 0;
-    e = newVector(e_x, e_y);
-
-    double _e_x = e_x * co - e_y * si;
-    double _e_y = e_x * si + e_y * co;
-    e = newVector(_e_x, _e_y);
-    e = newVector(c->x + _e_x, c->y + _e_y);
-
-
-
-    printf("\n\nNew Fractal Part\n");
-    printf("alpha: %d\t| %d\n", alpha, alphaNormalized);
-    printf("cycle: %d\n", cycles);
-    printf("h: %.0f\n\n", h);
-    printf("a: %.0f\t| %.0f\n", a->x, a->y);
-    printf("b: %.0f\t| %.0f\n", b->x, b->y);
-    printf("ab: %.0f\t| %.0f\n", ab->x, ab->y);
-    printf("c: %.0f\t| %.0f\n", c->x, c->y);
-    printf("d: %.0f\t| %.0f\n", d->x, d->y);
-    printf("e: %.0f\t| %.0f\n", e->x, e->y);
-    printf("|ce|: %.0f\n", length_ce);
-
-
-    char orig[19];
-    sprintf(orig, "%s", "{{path}}");
-    replace_str(orig, path, "{{path}}");
-
-    replace(a, "a");
-    replace(b, "b");
-    replace(c, "c");
-    replace(d, "d");
-    replace(e, "e");
-    e = newVector(c->x + _e_x, c->y + _e_y);
-
-    struct Vector *ce = newVector(c->x - e->x, c->y - e->y);
-    printf("|ce|: %.0f\n", vectorLength(ce));
-
-    double length_ab = vectorLength(ab);
-    printf("|ab|: %.0f\n", length_ab);
-
-    double factor = length_ce / length_ab;
-    return fractalPart(c, e, alpha, h * factor, ++cycles);
+    return
+        fractalPart(c, e, alpha + origAlpha, h, depth) +
+        fractalPart(e, d, alpha - origAlpha, h, depth);
 
 }
 
@@ -130,56 +49,27 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
-    newSvg = malloc(strlen(svg) + 1);
-    sprintf(newSvg, "%s", svg);
+    newSvg = malloc(strlen(svgTpl) + 1);
+    sprintf(newSvg, "%s", svgTpl);
 
     struct Vector *a = newVector(atoi(argv[1]), 0);
     struct Vector *b = newVector(atoi(argv[2]), 0);
 
-    unsigned int alpha = atoi(argv[3]);
-    unsigned int h = atoi(argv[4]);
+    origAlpha = atoi(argv[3]);
+    sinAlpha = sin(origAlpha * rad);
+    const unsigned int h = atoi(argv[4]);
 
 
-    fractalPart(a, b, alpha, h, 1);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    unsigned long start = 1000000 * tv.tv_sec + tv.tv_usec;
 
+    fractalPart(a, b, origAlpha, h, 1);
 
-    char rep[15];
-    char orig[19];
+    gettimeofday(&tv, NULL);
+    unsigned long end = 1000000 * tv.tv_sec + tv.tv_usec;
+    printf("Done in %lums\n", (end - start));
 
-    smallestX -= 10;
-    smallestY -= 10;
-    biggestX += 10;
-    biggestY += 10;
-
-    sprintf(rep, "%f", smallestX);
-    sprintf(orig, "%s", "{{minX}}");
-    replace_str(orig, rep, "");
-
-    sprintf(rep, "%f", smallestY);
-    sprintf(orig, "%s", "{{minY}}");
-    replace_str(orig, rep, "");
-
-    sprintf(rep, "%f", -smallestX + biggestX);
-    sprintf(orig, "%s", "{{width}}");
-    replace_str(orig, rep, "");
-
-    sprintf(rep, "%f", -smallestX + biggestX);
-    sprintf(orig, "%s", "{{width}}");
-    replace_str(orig, rep, "");
-
-    sprintf(rep, "%f", -smallestY + biggestY);
-    sprintf(orig, "%s", "{{height}}");
-    replace_str(orig, rep, "");
-
-    sprintf(rep, "%f", -smallestY + biggestY);
-    sprintf(orig, "%s", "{{height}}");
-    replace_str(orig, rep, "");
-
-    sprintf(orig, "%s", "\n\t{{path}}");
-    replace_str(orig, "", "");
-
-    FILE *fp = fopen("fractal.svg", "w");
-    fputs(newSvg, fp);
-    fclose(fp);
+    writeSvg();
 
 }
---
+++ ue02/svg.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+#include "svg.h"
+
+char svgTpl[1079] = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='100%' viewBox='{{minX}} {{minY}} {{width}} {{height}}'><style>svg { transform: translateZ(0) translate3d(0, 0, 0); backface-visibility: hidden; perspective: 1000; } path { fill: #fff; animation: rainbow 20s ease infinite; } .path-2 { animation-delay: 0.05s; } .path-3 { animation-delay: 0.15s; } .path-4 { animation-delay: 0.3s; } .path-5 { animation-delay: 0.5s; } .path-6 { animation-delay: 0.75s; } .path-7 { animation-delay: 1.15s; } .path-8 { animation-delay: 1.5s; } .path-9 { animation-delay: 1.9s; } .path-10 { animation-delay: 2.35s; } .path-11 { animation-delay: 2.85s; } .path-12 { animation-delay: 3.4s; } .path-13 { animation-delay: 4s; } .path-14 { animation-delay: 4.65s; } .path-15 { animation-delay: 5.35s; } @keyframes rainbow { 0% { fill: #DC001B; } 12.5% { fill: #FFEA2E; } 25% { fill: #228D1B; } 37.5% { fill: #1397F1; } 50% { fill: #5321A8; } 62.5% { fill: #1397F1; } 75% { fill: #228D1B; } 87.5% { fill: #FFEA2E; } 100% { fill: #DC001B; } } </style>\n\t{{path}}\n</svg>";
+
+char path[172] = "<path d='M {{a_x}} {{a_y}} L {{c_x}} {{c_y}} L {{e_x}} {{e_y}} L {{d_x}} {{d_y}} L {{b_x}} {{b_y}} z' class='path-{{depth}}' fill='#fff' stroke='#fff' stroke-width='1' />\n\t";
+
+
+int replace_str (char *orig, char *rep, char *after) {
+    // printf("replace %s with %s\n", orig, rep);
+
+    char buffer[2048000];
+    char *p;
+
+    if(!(p = strstr(newSvg, orig)))
+        return 0;
+
+    strncpy(buffer, newSvg, p - newSvg);
+    buffer[p - newSvg] = '\0';
+
+    sprintf(buffer + (p - newSvg), "%s%s%s", rep, after, p + strlen(orig));
+
+    newSvg = realloc(newSvg, strlen(buffer) + 1);
+    sprintf(newSvg, "%s", buffer);
+    return 1;
+}
+
+void replacePathStuff (struct Vector *v, char *name) {
+
+    char rep[30];
+    char orig[34];
+
+    if (v->x < smallestX) smallestX = v->x;
+    if (v->x > biggestX) biggestX = v->x;
+    if (v->y < smallestY) smallestY = v->y;
+    if (v->y > biggestY) biggestY = v->y;
+
+    sprintf(rep, "%f", v->x);
+    sprintf(orig, "{{%s_x}}", name);
+    replace_str(orig, rep, "");
+
+    sprintf(rep, "%f", v->y);
+    sprintf(orig, "{{%s_y}}", name);
+    replace_str(orig, rep, "");
+
+}
+
+void insertPath (struct Vector *a, struct Vector *b, struct Vector *c, struct Vector *d, struct Vector *e, int depth) {
+
+    char rep[30];
+    char orig[34];
+    sprintf(orig, "%s", "{{path}}");
+    replace_str(orig, path, "{{path}}");
+
+    sprintf(rep, "%d", depth);
+    sprintf(orig, "{{depth}}");
+    replace_str(orig, rep, "");
+
+    replacePathStuff(a, "a");
+    replacePathStuff(b, "b");
+    replacePathStuff(c, "c");
+    replacePathStuff(d, "d");
+    replacePathStuff(e, "e");
+
+}
+
+void writeSvg () {
+    char rep[30];
+    char orig[34];
+
+    smallestX -= 10;
+    smallestY -= 10;
+    biggestX += 10;
+    biggestY += 10;
+
+    sprintf(rep, "%f", smallestX);
+    sprintf(orig, "%s", "{{minX}}");
+    replace_str(orig, rep, "");
+
+    sprintf(rep, "%f", smallestY);
+    sprintf(orig, "%s", "{{minY}}");
+    replace_str(orig, rep, "");
+
+    sprintf(rep, "%f", -smallestX + biggestX);
+    sprintf(orig, "%s", "{{width}}");
+    replace_str(orig, rep, "");
+
+    sprintf(rep, "%f", -smallestY + biggestY);
+    sprintf(orig, "%s", "{{height}}");
+    replace_str(orig, rep, "");
+
+    sprintf(orig, "%s", "\n\t{{path}}");
+    replace_str(orig, "", "");
+
+    FILE *fp = fopen("fractal.svg", "w");
+    fputs(newSvg, fp);
+    fclose(fp);
+}
