7cf3b8b253d2a016fd3eb1eef4fc04deb0962a1b
Robert Wettstädt <robert.wett@gmail.com>
2016-05-30 23:58:34 +0200

    Add huffman prototype

--- ue05/_heapsort.h
+++ ue05/_heapsort.h
@@ -2,5 +2,7 @@
 #define HEAPSORT_H_
 
 int* _heapsort (int array[], const int size, unsigned long* modifications);
+void maxHeapify (int A[], const int i, const int heapSize, unsigned long* modifications);
+void hSwap (int A[], const int i, const int j);
 
 #endif // HEAPSORT_H_
