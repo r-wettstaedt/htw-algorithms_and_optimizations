2f91be9b4d9818f65be6917dedb57c82b85c3422
Sona Pecenakova <sona.pecenakova@gmail.com>
2016-06-01 09:01:33 +0200

    Huffman without file compression and decompression

--- ue05/huffman.c
+++ ue05/huffman.c
@@ -1,227 +1,256 @@
-/****************************************************************************/
-/*                A simple implementation of the Huffman coding             */
-/*                author: danielscocco@gmail.com                            */
-/*                                                                          */
-/****************************************************************************/
 
+// C program for Huffman Coding
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+// This constant can be avoided by explicitly calculating height of Huffman Tree
+#define MAX_TREE_HT 100
 #define len(x) ((int)log10(x)+1)
 
-/* Node of the huffman tree */
-struct node{
-    int value;
-    char letter;
-    struct node *left,*right;
+// A Huffman tree node
+struct MinHeapNode
+{
+    char data;  // One of the input characters
+    unsigned freq;  // Frequency of the character
+    struct MinHeapNode *left, *right; // Left and right child of this node
 };
 
-typedef struct node Node;
+// A Min Heap:  Collection of min heap (or Hufmman tree) nodes
+struct MinHeap
+{
+    unsigned size;    // Current size of min heap
+    unsigned capacity;   // capacity of min heap
+    struct MinHeapNode **array;  // Attay of minheap node pointers
+};
 
-/* 81 = 8.1%, 128 = 12.8% and so on. The 27th frequency is the space. Source is Wikipedia */
-int englishLetterFrequencies [27] = {81, 15, 28, 43, 128, 23, 20, 61, 71, 2, 1, 40, 24, 69, 76, 20, 1, 61, 64, 91, 28, 10, 24, 1, 20, 1, 130};
-
-/*finds and returns the small sub-tree in the forrest*/
-int findSmaller (Node *array[], int differentFrom){
-    int smaller;
-    int i = 0;
-
-    while (array[i]->value==-1)
-        i++;
-    smaller=i;
-    if (i==differentFrom){
-        i++;
-        while (array[i]->value==-1)
-            i++;
-        smaller=i;
-    }
-
-    for (i=1;i<27;i++){
-        if (array[i]->value==-1)
-            continue;
-        if (i==differentFrom)
-            continue;
-        if (array[i]->value<array[smaller]->value)
-            smaller = i;
-    }
-
-    return smaller;
+// A utility function allocate a new min heap node with given character
+// and frequency of the character
+struct MinHeapNode* newNode(char data, unsigned freq)
+{
+    struct MinHeapNode* temp =
+    (struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+    return temp;
 }
 
-/*builds the huffman tree and returns its address by reference*/
-void buildHuffmanTree(Node **tree){
-    Node *temp;
-    Node *array[27];
-    int i, subTrees = 27;
-    int smallOne,smallTwo;
-
-    for (i=0;i<27;i++){
-        array[i] = malloc(sizeof(Node));
-        array[i]->value = englishLetterFrequencies[i];
-        array[i]->letter = i;
-        array[i]->left = NULL;
-        array[i]->right = NULL;
-    }
-
-    while (subTrees>1){
-        smallOne=findSmaller(array,-1);
-        smallTwo=findSmaller(array,smallOne);
-        temp = array[smallOne];
-        array[smallOne] = malloc(sizeof(Node));
-        array[smallOne]->value=temp->value+array[smallTwo]->value;
-        array[smallOne]->letter=127;
-        array[smallOne]->left=array[smallTwo];
-        array[smallOne]->right=temp;
-        array[smallTwo]->value=-1;
-        subTrees--;
-    }
-
-    *tree = array[smallOne];
-
-return;
+// A utility function to create a min heap of given capacity
+struct MinHeap* createMinHeap(unsigned capacity)
+{
+    struct MinHeap* minHeap =
+    (struct MinHeap*) malloc(sizeof(struct MinHeap));
+    minHeap->size = 0;  // current size is 0
+    minHeap->capacity = capacity;
+    minHeap->array =
+    (struct MinHeapNode**)malloc(minHeap->capacity * sizeof(struct MinHeapNode*));
+    return minHeap;
 }
 
-/* builds the table with the bits for each letter. 1 stands for binary 0 and 2 for binary 1 (used to facilitate arithmetic)*/
-void fillTable(int codeTable[], Node *tree, int Code){
-    if (tree->letter<27)
-        codeTable[(int)tree->letter] = Code;
-    else{
-        fillTable(codeTable, tree->left, Code*10+1);
-        fillTable(codeTable, tree->right, Code*10+2);
-    }
+// A utility function to swap two min heap nodes
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b)
+{
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
 
-    return;
+// The standard minHeapify function.
+void minHeapify(struct MinHeap* minHeap, int idx)
+{
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+    
+    if (left < minHeap->size &&
+        minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+    
+    if (right < minHeap->size &&
+        minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+    
+    if (smallest != idx)
+    {
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+// A utility function to check if size of heap is 1 or not
+int isSizeOne(struct MinHeap* minHeap)
+{
+    return (minHeap->size == 1);
+}
+
+// A standard function to extract minimum value node from heap
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+    struct MinHeapNode* temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+    return temp;
+}
+
+// A utility function to insert a new node to Min Heap
+void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode)
+{
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+    while (i && minHeapNode->freq < minHeap->array[(i - 1)/2]->freq)
+    {
+        minHeap->array[i] = minHeap->array[(i - 1)/2];
+        i = (i - 1)/2;
+    }
+    minHeap->array[i] = minHeapNode;
+}
+
+// A standard funvtion to build min heap
+void buildMinHeap(struct MinHeap* minHeap)
+{
+    int n = minHeap->size - 1;
+    int i;
+    for (i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+
+// A utility function to print an array of size n
+void printArr(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("%d", arr[i]);
+    printf("\n");
+}
+
+// Utility function to check if this node is leaf
+int isLeaf(struct MinHeapNode* root)
+{
+    return !(root->left) && !(root->right) ;
+}
+
+// Creates a min heap of capacity equal to size and inserts all character of
+// data[] in min heap. Initially size of min heap is equal to capacity
+struct MinHeap* createAndBuildMinHeap(char data[], int freq[], int size)
+{
+    struct MinHeap* minHeap = createMinHeap(size);
+    for (int i = 0; i < size; ++i)
+        minHeap->array[i] = newNode(data[i], freq[i]);
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+    return minHeap;
+}
+
+// The main function that builds Huffman tree
+struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size)
+{
+    struct MinHeapNode *left, *right, *top;
+    
+    // Step 1: Create a min heap of capacity equal to size.  Initially, there are
+    // modes equal to size.
+    struct MinHeap* minHeap = createAndBuildMinHeap(data, freq, size);
+    
+    // Iterate while size of heap doesn't become 1
+    while (!isSizeOne(minHeap))
+    {
+        // Step 2: Extract the two minimum freq items from min heap
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+        
+        // Step 3:  Create a new internal node with frequency equal to the
+        // sum of the two nodes frequencies. Make the two extracted node as
+        // left and right children of this new node. Add this node to the min heap
+        // '$' is a special value for internal nodes, not used
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        insertMinHeap(minHeap, top);
+    }
+    
+    // Step 4: The remaining node is the root node and the tree is complete.
+    return extractMin(minHeap);
+}
+
+// Prints huffman codes from the root of Huffman Tree.  It uses arr[] to
+// store codes
+void printCodes(struct MinHeapNode* root, int arr[], int top)
+{
+    // Assign 0 to left edge and recur
+    if (root->left)
+    {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+    
+    // Assign 1 to right edge and recur
+    if (root->right)
+    {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+    
+    // If this is a leaf node, then it contains one of the input
+    // characters, print the character and its code from arr[]
+    if (isLeaf(root))
+    {
+        printf("%c: ", root->data);
+        printArr(arr, top);
+    }
+}
+
+// The main function that builds a Huffman Tree and print codes by traversing
+// the built Huffman Tree
+void HuffmanCodes(char data[], int freq[], int size)
+{
+    //  Construct Huffman Tree
+    struct MinHeapNode* root = buildHuffmanTree(data, freq, size);
+   
+    // Print Huffman codes using the Huffman tree built above
+    int arr[MAX_TREE_HT], top = 0;
+    printCodes(root, arr, top);
 }
 
 /*function to compress the input*/
-void compressFile(FILE *input, FILE *output, int codeTable[]){
-    char bit, c, x = 0;
-    int n,length,bitsLeft = 8;
-    int originalBits = 0, compressedBits = 0;
-
-    while ((c=fgetc(input))!=10){
-        originalBits++;
-        if (c==32){
-            length = len(codeTable[26]);
-            n = codeTable[26];
-        }
-        else{
-            length=len(codeTable[c-97]);
-            n = codeTable[c-97];
-        }
-
-        while (length>0){
-            compressedBits++;
-            bit = n % 10 - 1;
-            n /= 10;
-            x = x | bit;
-            bitsLeft--;
-            length--;
-            if (bitsLeft==0){
-                fputc(x,output);
-                x = 0;
-                bitsLeft = 8;
-            }
-            x = x << 1;
-        }
-    }
-
-    if (bitsLeft!=8){
-        x = x << (bitsLeft-1);
-        fputc(x,output);
-    }
-
-    /*print details of compression on the screen*/
-    fprintf(stderr,"Original bits = %d\n",originalBits*8);
-    fprintf(stderr,"Compressed bits = %d\n",compressedBits);
-    fprintf(stderr,"Saved %.2f%% of memory\n",((float)compressedBits/(originalBits*8))*100);
-
-    return;
+void compressFile(FILE *input, FILE *output){
+    //TODO
+    //compress file
 }
 
-/*function to decompress the input*/
-void decompressFile (FILE *input, FILE *output, Node *tree){
-    Node *current = tree;
-    char c,bit;
-    char mask = 1 << 7;
-    int i;
-
-    while ((c=fgetc(input))!=EOF){
-
-        for (i=0;i<8;i++){
-            bit = c & mask;
-            c = c << 1;
-            if (bit==0){
-                current = current->left;
-                if (current->letter!=127){
-                    if (current->letter==26)
-                        fputc(32, output);
-                    else
-                        fputc(current->letter+97,output);
-                    current = tree;
-                }
-            }
-
-            else{
-                current = current->right;
-                if (current->letter!=127){
-                    if (current->letter==26)
-                        fputc(32, output);
-                    else
-                        fputc(current->letter+97,output);
-                    current = tree;
-                }
-            }
-        }
-    }
-
-    return;
+void decompressFile(){
+	//TODO:
+	//decompress file with huffman codetable
 }
 
-/*invert the codes in codeTable2 so they can be used with mod operator by compressFile function*/
-void invertCodes(int codeTable[],int codeTable2[]){
-    int i, n, copy;
+// Driver program to test above functions
+int main()
+{	
+	
 
-    for (i=0;i<27;i++){
-        n = codeTable[i];
-        copy = 0;
-        while (n>0){
-            copy = copy * 10 + n %10;
-            n /= 10;
-        }
-        codeTable2[i]=copy;
-    }
-
-return;
-}
-
-int main(){
-    Node *tree;
-    int codeTable[27], codeTable2[27];
+	int freq [27] = {81, 15, 28, 43, 128, 23, 20, 61, 71, 2, 1, 40, 24, 69, 76, 20, 1, 61, 64, 91, 28, 10, 24, 1, 20, 1, 130};
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
+    int size = sizeof(letters)/sizeof(letters[0]);
+    HuffmanCodes(letters, freq, size);
+    
     int compress;
     char filename[20];
-    FILE *input, *output;
-
-    buildHuffmanTree(&tree);
-
-    fillTable(codeTable, tree, 0);
-
-    invertCodes(codeTable,codeTable2);
-
+    FILE *fileToCompress, *compressedFile;
+    
     /*get input details from user*/
     printf("Type the name of the file to process:\n");
     scanf("%s",filename);
     printf("Type 1 to compress and 2 to decompress:\n");
     scanf("%d",&compress);
 
-    input = fopen(filename, "r");
-    output = fopen("output.txt","w");
 
     if (compress==1)
-        compressFile(input,output,codeTable2);
-    else
-        decompressFile(input,output, tree);
-
+   		fileToCompress = fopen(filename, "r");
+    	compressedFile = fopen("compressedFile.txt","w");
+        compressFile(fileToCompress,compressedFile);
+    //else
+       // decompressFile(compressedFile,decompressedFile);
+    
+    
     return 0;
 }
