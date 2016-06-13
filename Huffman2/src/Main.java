24e650abfcbfba77941822743336e185d503014d
Sona Pecenakova <sona.pecenakova@gmail.com>
2016-06-13 17:53:41 +0200

    compress method added

--- Huffman2/src/Main.java
+++ Huffman2/src/Main.java
@@ -2,8 +2,10 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.util.HashMap;
 
 
@@ -35,31 +37,46 @@ public class Main {
 		return freq;
 	}
 	
-	public void compressFile(File inputFile, File outputFile, HashMap<Character, String> codeTable) throws IOException{
+	public static void compressFile(File inputFile, File outputFile, HashMap<Character, String> codeTable) throws IOException{
 		InputStream inStream = new BufferedInputStream(new FileInputStream(inputFile));
 		try {
+			String outputString = "";
 			int c;
 			while ((c = inStream.read()) != -1) {
 				char ch = (char)c;
 				String code = codeTable.get(ch);
-				
-				
-				
-				for(int i = 0; i < data.length; i++){
-					if(data[i] == ch){
-					}
-				}
+				outputString += code;
 			}
+			
+			//fill the last unfilled spots of the last bit with zeros
+			int bitsLeftToFill = outputString.length()%8;
+			for(int i = 0; i < bitsLeftToFill; i++){
+				outputString += "0";
+			}
+			
+			byte[] b = new BigInteger(outputString, 2).toByteArray();
+			
+			
+			FileOutputStream fos = new FileOutputStream(outputFile);
+			fos.write(b);
+			fos.close();
+			
+			System.out.println("converted");
 		} finally {
 			inStream.close();
 		}
 		
 	}
 	
+	public static void decompressFile(File toDecompress, File decompressed, HashMap<Character, String> codeTable){
+		//TODO
+		//Decompress file
+	}
+	
 	public static void main(String[] args) throws IOException{
 		//get letter frequencies from file
-		File file = new File("/Users/sonynka/Documents/HTW/Algorithms/Huffman2/input.txt");
-		int[] freq = getFrequencies(file);
+		File originalFile = new File("/Users/sonynka/Documents/HTW/Algorithms/Huffman2/input.txt");
+		int[] freq = getFrequencies(originalFile);
 		int size = freq.length;
 		
 		HuffmanMinHeap minHeap = new HuffmanMinHeap(data, freq, size);
@@ -73,6 +90,13 @@ public class Main {
 		minHeap.printCodes(root, arr, top);
 		HashMap<Character, String> codeTable = new HashMap<Character, String>();
 		minHeap.getCodes(root, arr, top, codeTable);
+		
+		File compressedFile = new File("/Users/sonynka/Documents/HTW/Algorithms/Huffman2/output");
+		compressFile(originalFile, compressedFile, codeTable);
+		
+		File decompressedFile = new File("/Users/sonynka/Documents/HTW/Algorithms/Huffman2/decompressedFile.txt");
+		decompressFile(compressedFile, decompressedFile, codeTable);
+		
 		
 		System.out.println("done");
 		
