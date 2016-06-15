2e776db1822ad654ee7afc089180ff2ae2ed20f0
Sona Pecenakova <sona.pecenakova@junolaptop281.de.bonial.lan>
2016-06-15 14:47:25 +0200

    decompress method added

--- Huffman2/src/Main.java
+++ Huffman2/src/Main.java
@@ -3,16 +3,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.nio.file.Files;
 import java.util.HashMap;
+import java.util.Set;
 
 
 public class Main {
 	
-	//public static char[] data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
-	public static char[] data = {'a', 'b', 'c', 'd', 'e', 'f'};
+	public static char[] data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};
+//	public static char[] data = {'a', 'b', 'c', 'd', 'e', 'f'};
 	
 	
 	private static int[] getFrequencies(File file) throws IOException {
@@ -78,14 +82,67 @@ public class Main {
 		
 	}
 	
-	public static void decompressFile(File toDecompress, File decompressed, HashMap<Character, String> codeTable){
-		//TODO
-		//Decompress file
+	public static void decompressFile(File toDecompress, File decompressed, HashMap<Character, String> codeTable) throws IOException{
+		HashMap<String, Character> revCodeTable = revertCodeTable(codeTable);
+		
+		byte[] fileBytes = Files.readAllBytes(toDecompress.toPath());
+		InputStream inStream = new BufferedInputStream(new FileInputStream(toDecompress));
+		try {
+			String outputString = "";
+			String readCode = "";
+			String code = "";
+			
+			for(int i = 0; i < fileBytes.length; i++){
+				byte by = fileBytes[i];
+				String byteStr = Integer.toBinaryString(by & 0xFF);
+				if(byteStr.length() < 8){
+					//get the most significant bit - zero (sign)
+					byteStr = "0" + byteStr;
+				}
+				
+				code += byteStr;
+				
+			}
+			
+				
+			for(int i = 0; i < code.length(); i++){
+				char c = code.charAt(i);
+				readCode += c;
+				if(revCodeTable.containsKey(readCode)){
+					outputString += revCodeTable.get(readCode);
+					readCode = "";
+				}
+			}
+		
+			FileWriter out = new FileWriter(decompressed);
+			out.write(outputString);
+			out.close();
+			
+			System.out.println("converted");
+		} finally {
+			inStream.close();
+		}
+		
+		
+	}
+	
+	public static HashMap<String, Character> revertCodeTable(HashMap<Character, String> origCodeTable){
+		HashMap<String, Character> revCodeTable = new HashMap<String, Character>();
+		
+		Set<Character> set = origCodeTable.keySet();
+		Object[] keys = set.toArray();
+		for(int i = 0; i < keys.length; i++){
+			char key = (char)keys[i];
+			String value = origCodeTable.get(key);
+			revCodeTable.put(value, key);
+		}
+		
+		return revCodeTable;
 	}
 	
 	public static void main(String[] args) throws IOException{
 		//get letter frequencies from file
-		File originalFile = new File("input2.txt");
+		File originalFile = new File("input.txt");
 		int[] freq = getFrequencies(originalFile);
 		int size = freq.length;
 		
@@ -101,7 +158,7 @@ public class Main {
 		HashMap<Character, String> codeTable = new HashMap<Character, String>();
 		minHeap.getCodes(root, arr, top, codeTable);
 		
-		File compressedFile = new File("output");
+		File compressedFile = new File("compressedFile");
 		compressFile(originalFile, compressedFile, codeTable);
 		
 		File decompressedFile = new File("decompressedFile.txt");
