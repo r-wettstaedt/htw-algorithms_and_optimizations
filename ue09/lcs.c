4e65757c1eb1696d5165640c41df9e8e22e2d65c
Sona Pecenakova <sona.pecenakova@gmail.com>
2016-07-13 21:35:55 +0200

    fix LCS

--- ue09/lcs.c
+++ ue09/lcs.c
@@ -6,56 +6,53 @@ char x[20],y[20],b[20][20];
 
 void print(int i,int j)
 {
-                if(i==0 || j==0)
-                    return;
-
-                if(b[i][j]=='p'){
-                    print(i-1,j-1);
-                    printf("%c",x[i]);
-                }
-                else if(b[i][j]=='o')
-                    print(i-1,j);
-                else
-                    print(i,j-1);
+    if(i==0 || j==0)
+    	return;
+    if(b[i][j]=='p'){
+        print(i-1,j-1);
+        printf("%c",x[i-1]);
+    }
+    else if(b[i][j]=='o')
+        print(i-1,j);
+    else
+        print(i,j-1);
 }
 
 void lcs()
 {
-                m=strlen(x);
-                n=strlen(y);
+    m=strlen(x);
+	n=strlen(y);
 
-                for(i=1;i<=m;i++)
-                                c[i][0]=0;
-                for(j=1;j<=n;j++)
-                                c[0][j]=0;
+    for(i=0;i<=m;i++){
+        c[i][0]=0;
+    }
+    for(i=0;i<=n;i++){
+        c[0][i]=0;
+    }
 
-                //l = links, o = oben, p = linksoben
-                for(i=1;i<=m;i++)
-                	for(j=1;j<=n;j++){
-                		if(x[i]==y[j]){
-                             c[i][j]=c[i-1][j-1]+1;
-                             b[i][j]='p';
-                        }
-                        else if(c[i-1][j]>=c[i][j-1]) {
-            				c[i][j]=c[i-1][j];
-                            b[i][j]='o';
-                    	}
-                        else{
-                        	c[i][j]=c[i][j-1];
-                            b[i][j]='l';
-                        }
-                }
+    //p links oben, o oben, l links
+    for(i=1;i<=m;i++)
+        for(j=1;j<=n;j++){
+        	if(x[i-1]==y[j-1]){
+            	c[i][j]=c[i-1][j-1]+1;
+            	b[i][j]='p';
+            } else if(c[i-1][j]>=c[i][j-1]){
+           		c[i][j]=c[i-1][j];
+       			b[i][j]='o';
+            } else {
+                c[i][j]=c[i][j-1];
+                b[i][j]='l';
+            }
+    }
 }
 
 int main()
 {
-                printf("Erste Sequenz: ");
-                scanf("%s",x);
-                printf("Zweite Sequenz:");
-                scanf("%s",y);
-                printf("\nThe Longest Common Subsequence is ");
-                lcs();
-                print(m,n);
-                printf("\n");
-        		return 0;
+        scanf("%s",x);
+        scanf("%s",y);
+        printf("\nThe Longest Common Subsequence is ");
+        lcs();
+        print(m,n);
+        printf("\n");
+        return 0;
 }
